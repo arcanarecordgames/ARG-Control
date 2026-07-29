b73483a695bd2ae11934a7a2d1fcc66e612e9b00
arcanarecordgames <yutoyuto030714@gmail.com>
2026-07-29 20:47:39 +0900

    admin.js を更新

--- js/admin.js
+++ js/admin.js
@@ -3,7 +3,12 @@
 // Admin Authorization System
 // =================================
 
+import { db } from "./firebase-config.js";
 
+import {
+  ref,
+  get
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const memberData =
 
@@ -114,3 +119,78 @@ adminLevel.textContent =
 member.access_level;
 
 }
+// MEMBER DATABASE取得
+
+const memberList =
+document.getElementById(
+"memberList"
+);
+
+
+get(ref(db, "members"))
+.then((snapshot)=>{
+
+
+if(snapshot.exists()){
+
+
+const members =
+snapshot.val();
+
+
+memberList.innerHTML = "";
+
+
+Object.keys(members).forEach((id)=>{
+
+
+const data =
+members[id];
+
+
+memberList.innerHTML += `
+
+<tr>
+
+<td>${id}</td>
+
+<td>${data.username}</td>
+
+<td>${data.access_level}</td>
+
+<td>${data.status}</td>
+
+</tr>
+
+`;
+
+
+});
+
+
+}else{
+
+
+memberList.innerHTML = `
+
+<tr>
+
+<td colspan="4">
+NO DATA
+</td>
+
+</tr>
+
+`;
+
+}
+
+
+})
+.catch((error)=>{
+
+
+console.error(error);
+
+
+});
